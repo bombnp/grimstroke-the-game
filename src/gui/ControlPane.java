979683f6d2f716857e87acd2bb6cdc077b07edc1
bombnp <bomb.NP@gmail.com>
2020-04-22 22:39:57 +0700

    Refactored some more codes

--- src/gui/ControlPane.java
+++ src/gui/ControlPane.java
@@ -8,11 +8,11 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
 public class ControlPane extends VBox {
-	private static final int ControlPaneSize = 100;
+	private static final int CONTROL_PANE_SIZE = 100;
 
     public ControlPane() {
         this.setBackground(new Background(new BackgroundFill(Color.gray(0.5), CornerRadii.EMPTY, Insets.EMPTY)));
-        this.setPrefWidth(ControlPaneSize);
+        this.setPrefWidth(CONTROL_PANE_SIZE);
         this.setSpacing(10);
     }
     public void addTower(int bgSprite, String name, String damage, String rates, String range, boolean isTool) {
@@ -20,6 +20,6 @@ public class ControlPane extends VBox {
     	this.getChildren().add(cell);
     }
     public static int getControlPaneSize() {
-    	return ControlPaneSize;
+    	return CONTROL_PANE_SIZE;
     }
 }
