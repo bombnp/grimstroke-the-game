e9b619376f4afcb7a8b6e8b29ebbf78745ed7cc9
Uttanon <uttanon.aug@gmail.com>
2020-05-14 01:09:21 +0700

    Add winning window

--- src/gui/WinGamePane.java
+++ src/gui/WinGamePane.java
@@ -2,23 +2,20 @@ package gui;
 
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
-import javafx.scene.layout.VBox;
-import logic.GameController;
+import javafx.scene.control.Alert.AlertType;
 
-
-public class GameOverPane extends VBox{
-	public GameOverPane() {
+public class WinGamePane {
+	public WinGamePane() {
 		createPreset();
 	}
 	public void createPreset() {
-		Alert alert = new Alert(AlertType.ERROR);
-		alert.setTitle("Game Over!");
-		alert.setHeaderText("Your health point is below or equal zero");
-		alert.setContentText("Choose your option:");
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Congratulations!");
+		alert.setHeaderText(null);
+		alert.setContentText("There're no more waves. You win!");
 		ButtonType NewGame = new ButtonType("New Game");
-		ButtonType ExitGame = new ButtonType("Exit Game");
+		ButtonType ExitGame = new ButtonType("Close Game");
 		alert.getButtonTypes().setAll(NewGame,ExitGame);
 		Platform.runLater(() -> alert.showAndWait().ifPresent(response -> {
 			if (response == NewGame) {
--- src/logic/MinionWaveController.java
+++ src/logic/MinionWaveController.java
@@ -2,18 +2,22 @@ package logic;
 
 import database.Database;
 import database.MinionData;
+import database.WaveData;
 import entity.minion.AirUnit;
 import entity.minion.GroundUnit;
+import gui.WinGamePane;
 import javafx.application.Platform;
+import javafx.scene.chart.PieChart.Data;
 
 public class MinionWaveController{
-	private int waveNumber;
+	public static int waveNumber;
 	private final int TIME_BETWEEN_SPAWN = 500; // ms
 
 	public MinionWaveController() {
 		waveNumber = 0;
 	}
 	public void generateNextWave() {
+		if(waveNumber+1 <= Database.waves.length-1) {
 		new Thread(() -> {
 			for(int i = 0 ; i < 8 ; i++) {
 				for(int j = 0; j < Database.waves[waveNumber].minionsCount[i] ; j++) {
@@ -39,5 +43,8 @@ public class MinionWaveController{
 		}).start();
 
 		waveNumber++;
+		}else {
+			new WinGamePane();
+		}
 	}
 }
